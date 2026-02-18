14b6541c280f3368c1e517cf7db8ca17642d709e
israel <israelashenafi29@gmail.com>
2026-02-18 13:48:14 -0800

    ADD: user get email repository

--- internals/User/repository.go
+++ internals/User/repository.go
@@ -1,1 +1,21 @@
 package user
+
+import (
+	models "github.com/izzy-Ti/PromptRecruit/internals/Models"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+func (r *UserRepository) GetByEmail(email string) (*models.User, error) {
+	var user models.User
+	if err := r.db.Where("email = ?", email).Find(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
