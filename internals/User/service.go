a07dc4646a2db586745b2d9b1ed2231d21da4681
israel <israelashenafi29@gmail.com>
2026-02-21 09:03:32 -0800

    ADD: user login service

--- internals/User/service.go
+++ internals/User/service.go
@@ -2,8 +2,11 @@ package user
 
 import (
 	"errors"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	models "github.com/izzy-Ti/PromptRecruit/internals/Models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -15,9 +18,24 @@ func NewUserService(repo *UserRepository, secret string) *UserService {
 	return &UserService{repo: repo, jwtSecrete: []byte(secret)}
 }
 
-func (s *UserService) Authenticate(email, password string) (*models.User, string, error) {
-	if user, err := s.repo.GetByEmail(email); err != nil {
+func (s *UserService) Login(email, password string) (*models.User, string, error) {
+	user, err := s.repo.GetByEmail(email)
+	if err != nil {
 		return nil, "", errors.New("invalid email or password")
 	}
-
+	if !user.IsAccVerified {
+		return nil, "", errors.New("please verify your account")
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return nil, "", errors.New("incorrect credentials")
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	tokenString, err := token.SignedString(s.jwtSecrete)
+	if err != nil {
+		return nil, "", err
+	}
+	return user, tokenString, nil
 }
