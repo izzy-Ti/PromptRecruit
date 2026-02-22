53729f5a56f95554bdc443205d48e60939700c0f
israel <israelashenafi29@gmail.com>
2026-02-22 08:17:28 -0800

    FEAT: routing of user authentication

--- internals/Server/router.go
+++ internals/Server/router.go
@@ -1,10 +1,8 @@
 package server
 
 import (
-	"net/http"
-
+	//"github.com/gorilla/mux"
 	"github.com/gorilla/mux"
-	"github.com/izzy-Ti/PromptRecruit/internals/Middleware"
 	user "github.com/izzy-Ti/PromptRecruit/internals/User"
 	"gorm.io/gorm"
 )
@@ -16,5 +14,12 @@ func Auth(r *mux.Router, db *gorm.DB, jwtSecret string) {
 
 	userAuth := r.PathPrefix("/user").Subrouter()
 	userAuth.HandleFunc("/Login", h.Login).Methods("POST")
-	userAuth.Handle("/verify", Middleware.IsAuth(svc, http.HandlerFunc(h.VerifyOTP)))
+	userAuth.HandleFunc("/register", h.Register).Methods("POST")
+	userAuth.HandleFunc("/logout", h.Logout).Methods("POST")
+	userAuth.HandleFunc("/SendVerifyOTP", h.SendVerifyOTP).Methods("POST")
+	userAuth.HandleFunc("/VerifyOTP", h.VerifyOTP).Methods("POST")
+	userAuth.HandleFunc("/SendResetOTP", h.SendResetOTP).Methods("POST")
+	userAuth.HandleFunc("/ResetPassword", h.ResetPassword).Methods("POST")
+	userAuth.HandleFunc("/GoogleAuth", h.GoogleAuth).Methods("POST")
+	//userAuth.Handle("/verify", Middleware.IsAuth(svc, http.HandlerFunc(h.VerifyOTP)))
 }
