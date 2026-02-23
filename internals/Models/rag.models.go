afb5919d5f6b42305635db5f7e8299996d2e33c3
israel <israelashenafi29@gmail.com>
2026-02-23 06:18:23 -0800

    FEAT: embedder integration of plane text and chunking

---
+++ internals/Models/rag.models.go
@@ -0,0 +1,10 @@
+package models
+
+import "github.com/pgvector/pgvector-go"
+
+type KnowledgeChunk struct {
+	ID      string          `gorm:"primaryKey"`
+	Content string          `gorm:"not null"`
+	Vector  pgvector.Vector `gorm:"type:vector(1024);not null"`
+	Source  string          `gorm:""`
+}
---
+++ internals/Rag/embedder.go
@@ -0,0 +1,91 @@
+package rag
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	models "github.com/izzy-Ti/PromptRecruit/internals/Models"
+	"github.com/pgvector/pgvector-go"
+	"gorm.io/gorm"
+)
+
+func EmbedText(txt string) ([]float32, error) {
+	body := map[string]interface{}{
+		"model": "voyage-3-large",
+		"input": []string{txt},
+	}
+	json_body, _ := json.Marshal(body)
+	req, err := http.NewRequest(
+		"POST",
+		"https://api.voyageai.com/v1/embeddings",
+		bytes.NewBuffer(json_body),
+	)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("VOYAGE_API_KEY"))
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(res.Body)
+		return nil, fmt.Errorf("voyage error: %s", string(body))
+	}
+	var result struct {
+		Data []struct {
+			Embedding []float32 `json:"embedding"`
+		} `json:"data"`
+	}
+	json.NewDecoder(res.Body).Decode(&result)
+	if len(result.Data) == 0 {
+		return nil, fmt.Errorf("no embedding returned")
+	}
+	var Vector [][]float32
+	for _, d := range result.Data {
+		Vector = append(Vector, d.Embedding)
+	}
+	return result.Data[0].Embedding, nil
+}
+func ChunkText(text string, size int) []string {
+	var chunks []string
+	runes := []rune(text)
+
+	for i := 0; i < len(runes); i += size {
+		end := i + size
+
+		if end > len(runes) {
+			end = len(runes)
+		}
+		chunks = append(chunks, string(runes[i:end]))
+	}
+
+	return chunks
+}
+func SaveProcessedChunks(db *gorm.DB, text string) error {
+	chunks := ChunkText(text, 500)
+
+	for i, chunkContent := range chunks {
+		vectorValues, err := EmbedText(text)
+		if err != nil {
+			return fmt.Errorf("failed to embed chunk %d: %v", i, err)
+		}
+		saveRecord := models.KnowledgeChunk{
+			Content: chunkContent,
+			Source:  "PDF",
+			Vector:  pgvector.NewVector(vectorValues),
+		}
+
+		if err := db.Create(&saveRecord).Error; err != nil {
+			return fmt.Errorf("failed to save chunk %d to db: %v", i, err)
+		}
+	}
+	return nil
+}
